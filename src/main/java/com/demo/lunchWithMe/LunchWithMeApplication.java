086ec5936c1089994c255fd101819be6426980cf
Azderica <mh97888@gmail.com>
2025-01-30 14:06:08 +0900

    first init

---
+++ src/main/java/com/demo/lunchWithMe/LunchWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.demo.lunchWithMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchWithMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchWithMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/lunchWithMe/LunchWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.lunchWithMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunchWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
